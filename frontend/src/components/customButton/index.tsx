e47d220f6acce1a3b07a06436dc681725c294456
Silvia Roldán <silviarf2902@gamilcom>
2024-02-25 15:01:31 +0100

    feat(CustomButton): add file upload behaviour

--- frontend/src/components/customButton/index.tsx
+++ frontend/src/components/customButton/index.tsx
@@ -1,14 +1,26 @@
 import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+export const UploadFile = styled('input')({
+  position: 'absolute',
+  opacity: 0,
+  cursor: 'pointer',
+});
 
 type style = {
   backgroundColor: string;
   color: string;
+  border?: string;
+  width?: string;
+  height?: string;
 };
 
 type CustomButtonProps = {
   label?: string;
   buttonColors?: style;
   onClick?: () => void;
+  file?: boolean;
   testId?: string;
 };
 
@@ -16,16 +28,20 @@ const CustomButton = ({
   label,
   buttonColors,
   onClick,
+  file = false,
   testId,
 }: CustomButtonProps) => {
   return (
     <Button
-      children={label}
       style={buttonColors}
       sx={{ textTransform: 'none' }}
       onClick={onClick}
       data-testid={testId}
-    />
+      startIcon={file && <CloudUploadIcon />}
+    >
+      {label}
+      {file && <UploadFile type="file" />}
+    </Button>
   );
 };
 
