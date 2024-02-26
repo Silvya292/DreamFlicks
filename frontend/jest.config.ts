2dd2d121c086da1790dc637bbea538eaa41cf46a
Silvia Roldán <silviarf2902@gamilcom>
2024-02-26 20:35:54 +0100

    chore(multiple files): prettier format

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,6 +3,7 @@ export default {
   displayName: 'frontend',
   preset: '../jest.preset.js',
   coverageDirectory: '../coverage/frontend',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
