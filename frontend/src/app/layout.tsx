86c65fc9814a2bdee3444c55b2752159f3f50333
Silvia Roldán <silviarf2902@gamilcom>
2024-02-28 19:38:52 +0100

    feat(layout): add common layout to project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,49 @@
+import { AppBar, Container, Toolbar } from '@mui/material';
+import dfLogo from '../assets/dfLogo.png';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+import UserButton from '../components/userButton';
+
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: '#ffffff',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '4rem',
+  zIndex: 1,
+});
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0 1.5rem',
+});
+
+const StyledLogo = styled('img')({
+  position: 'absolute',
+  width: '12rem',
+  height: 'auto',
+  top: '0.5rem',
+  cursor: 'pointer',
+});
+
+const Layout = () => {
+  const user = 'Usuario';
+
+  return (
+    <StyledAppBar>
+      <Container disableGutters maxWidth={false}>
+        <StyledToolbar disableGutters>
+          <Link to={'/'}>
+            <StyledLogo alt="DreamFlicks Logo" src={dfLogo} />
+          </Link>
+          <UserButton label={user} registered={false} />
+        </StyledToolbar>
+      </Container>
+    </StyledAppBar>
+  );
+};
+
+export default Layout;
