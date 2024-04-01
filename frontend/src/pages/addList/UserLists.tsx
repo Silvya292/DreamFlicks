f220d5e9026331428c5fceb5c46489b7c6ae95bd
Silvia Roldán <silviarf2902@gamilcom>
2024-04-01 19:49:54 +0200

    feat(userLists): add collaborative behaviour

--- frontend/src/pages/addList/UserLists.tsx
+++ frontend/src/pages/addList/UserLists.tsx
@@ -1,5 +1,7 @@
 import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import GroupsIcon from '@mui/icons-material/Groups';
+import CircleIcon from '@mui/icons-material/Circle';
 
 const StyledCardContent = styled(CardContent)({
   fontSize: '1rem',
@@ -15,6 +17,7 @@ interface UserListsProps {
     listTitle: string;
     listDescription: string;
     listImage: string;
+    isCollaborative: boolean;
   }[];
 }
 
@@ -45,8 +48,37 @@ const UserLists = ({ data }: UserListsProps) => {
                 paddingLeft: '0.4rem',
               }}
             >
+              {item.isCollaborative && (
+                <>
+                  <CircleIcon
+                    sx={{
+                      fontSize: 50,
+                      color: '#ffffff',
+                    }}
+                    style={{
+                      position: 'absolute',
+                      top: '0.5rem',
+                      left: '0.5rem',
+                      zIndex: 1,
+                    }}
+                  />
+                  <GroupsIcon
+                    sx={{ fontSize: 35 }}
+                    style={{
+                      position: 'absolute',
+                      top: '0.85rem',
+                      left: '1rem',
+                      zIndex: 2,
+                    }}
+                  />
+                </>
+              )}
               <CardMedia
-                style={{ borderRadius: '2%', objectFit: 'cover' }}
+                style={{
+                  borderRadius: '2%',
+                  objectFit: 'cover',
+                  position: 'relative',
+                }}
                 component="img"
                 height="300rem"
                 image={item.listImage}
