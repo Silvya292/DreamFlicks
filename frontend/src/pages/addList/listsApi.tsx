886bb85c804e7960ec7e3c895c85b207f57aa12a
Silvia Roldán <silviarf2902@gamilcom>
2024-04-01 19:48:24 +0200

    feat(listsApi): add createList method

--- frontend/src/pages/addList/listsApi.tsx
+++ frontend/src/pages/addList/listsApi.tsx
@@ -6,4 +6,9 @@ const getLists = async () => {
   return response.data;
 };
 
-export default { getLists };
+const createList = async (list: any) => {
+  const API_URL = 'http://localhost:3000/api';
+  const response = await axios.post(`${API_URL}/lists/createList`, list);
+  return response.data;
+};
+export default { getLists, createList };
