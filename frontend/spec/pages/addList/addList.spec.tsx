ccf790822b80931ad7edef08064ec005ba3bbe7f
Silvia Roldán <silviarf2902@gamilcom>
2024-04-15 22:48:22 +0200

    feat(AddList): add tests

--- frontend/spec/pages/addList/addList.spec.tsx
+++ frontend/spec/pages/addList/addList.spec.tsx
@@ -2,9 +2,9 @@ import { describe, expect, it } from '@jest/globals';
 import '@testing-library/jest-dom/jest-globals';
 import { fireEvent, render, screen } from '@testing-library/react';
 
-import AddList from '../../../src/pages/addList';
+import AddList from '../../../src/pages/showLists';
 
-describe('addList Action', () => {
+describe('showLists Action', () => {
   it('should render a modal when a button is clicked', () => {
     render(<AddList />);
     const button = screen.getByTestId('addListButton');
