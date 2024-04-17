54f3e269e59d1a5a3d9f436a97b4233205ae1fdc
Silvia Roldán <silviarf2902@gamilcom>
2024-04-17 19:15:55 +0200

    chore(userLists): extract styles

--- frontend/src/pages/showLists/UserLists.tsx
+++ frontend/src/pages/showLists/UserLists.tsx
@@ -9,6 +9,13 @@ const StyledLink = styled(Link)({
   color: 'inherit',
 });
 
+const CardContainer = styled('div')({
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '1.5rem',
+  paddingTop: '1rem',
+});
+
 const StyledCardContent = styled(CardContent)({
   fontSize: '1rem',
   fontWeight: 'bold',
@@ -30,75 +37,80 @@ interface UserListsProps {
 
 const UserLists = ({ data }: UserListsProps) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: '1.5rem',
-        paddingTop: '1rem',
-      }}
-    >
-      {data.map((item, index) => (
-        <StyledLink key={item.listId} to={`/user/id/list/${item.listId}`}>
-          <Card
-            key={index}
-            style={{
-              backgroundColor: '#f4f3f3',
-              height: '22rem',
-              width: '13rem',
-            }}
-          >
-            <CardActionArea>
-              <div
-                style={{
-                  paddingTop: '0.2rem',
-                  paddingRight: '0.2rem',
-                  paddingLeft: '0.2rem',
-                }}
-              >
-                {item.isCollaborative && (
-                  <>
-                    <CircleIcon
-                      sx={{
-                        fontSize: 50,
-                        color: '#ffffff',
-                      }}
-                      style={{
-                        position: 'absolute',
-                        top: '0.5rem',
-                        left: '0.5rem',
-                        zIndex: 1,
-                      }}
-                    />
-                    <GroupsIcon
-                      sx={{ fontSize: 35 }}
-                      style={{
-                        position: 'absolute',
-                        top: '0.85rem',
-                        left: '1rem',
-                        zIndex: 2,
-                      }}
-                    />
-                  </>
-                )}
-                <CardMedia
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin={''}
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      <CardContainer>
+        {data.map((item, index) => (
+          <StyledLink key={item.listId} to={`/user/id/list/${item.listId}`}>
+            <Card
+              key={index}
+              style={{
+                backgroundColor: '#f4f3f3',
+                height: '22rem',
+                width: '13rem',
+              }}
+            >
+              <CardActionArea>
+                <div
                   style={{
-                    borderRadius: '2%',
-                    objectFit: 'cover',
-                    position: 'relative',
+                    paddingTop: '0.2rem',
+                    paddingRight: '0.2rem',
+                    paddingLeft: '0.2rem',
                   }}
-                  component="img"
-                  height="300rem"
-                  image={item.listImage}
-                  alt={item.listTitle}
-                />
-              </div>
-              <StyledCardContent>{item.listTitle}</StyledCardContent>
-            </CardActionArea>
-          </Card>
-        </StyledLink>
-      ))}
-    </div>
+                >
+                  {item.isCollaborative && (
+                    <>
+                      <CircleIcon
+                        sx={{
+                          fontSize: 50,
+                          color: '#ffffff',
+                        }}
+                        style={{
+                          position: 'absolute',
+                          top: '0.5rem',
+                          left: '0.5rem',
+                          zIndex: 1,
+                        }}
+                      />
+                      <GroupsIcon
+                        sx={{ fontSize: 35 }}
+                        style={{
+                          position: 'absolute',
+                          top: '0.85rem',
+                          left: '1rem',
+                          zIndex: 2,
+                        }}
+                      />
+                    </>
+                  )}
+                  <CardMedia
+                    style={{
+                      borderRadius: '2%',
+                      objectFit: 'cover',
+                      position: 'relative',
+                    }}
+                    component="img"
+                    height="300rem"
+                    image={item.listImage}
+                    alt={item.listTitle}
+                  />
+                </div>
+                <StyledCardContent>{item.listTitle}</StyledCardContent>
+              </CardActionArea>
+            </Card>
+          </StyledLink>
+        ))}
+      </CardContainer>
+    </>
   );
 };
 
