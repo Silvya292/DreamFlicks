8686fa9252dbcb2080ec94b7f34adb3eef152287
Silvia Roldán <silviarf2902@gamilcom>
2024-04-17 18:24:43 +0200

    refactor(filmInfo): extract method

--- frontend/src/pages/filmInfo/index.tsx
+++ frontend/src/pages/filmInfo/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import api from './filmInfoApi';
 import FilmDetails from './FilmDetails';
+import { transformDate } from '../../components/transformDate';
 
 export interface FilmInfo {
   data: {
@@ -12,26 +13,6 @@ export interface FilmInfo {
     release_date: string;
     genres: string;
   };
-}
-
-function transformDate(date: string) {
-  const [year, month, day] = date.split('-');
-  const months = [
-    'ene',
-    'feb',
-    'mar',
-    'abr',
-    'may',
-    'jun',
-    'jul',
-    'ago',
-    'sep',
-    'oct',
-    'nov',
-    'dic',
-  ];
-  const monthName = months[Number(month) - 1];
-  return `${day} ${monthName} ${year}`;
 }
 
 const FilmInfo = () => {
