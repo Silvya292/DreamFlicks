473c10ab4d3a550645d6c6ffe45946733fc1ce76
Silvia Roldán <silviarf2902@gamilcom>
2024-04-17 18:18:37 +0200

    feat(Description): add component

---
+++ frontend/src/components/description/index.tsx
@@ -0,0 +1,34 @@
+interface DescriptionProps {
+  descriptionText: string;
+}
+
+const Description = ({ descriptionText }: DescriptionProps) => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin={''}
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      <div
+        style={{
+          fontFamily: 'Roboto, sans-serif',
+          fontSize: '1rem',
+          textAlign: 'center',
+          padding: '1rem',
+          width: '75%',
+          color: '#4d4f4d',
+        }}
+      >
+        {descriptionText}
+      </div>
+    </>
+  );
+};
+
+export default Description;
