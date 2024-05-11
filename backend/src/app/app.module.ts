db984dd981fccaa17ca2c65d3b6de6188f80804a
Silvia Roldán <silviarf2902@gamilcom>
2024-05-11 20:10:14 +0200

    feat(app): add mongoose

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { AppLoggerMiddleware } from './app.middleware';
 import { FilmModule } from '../films/film.module';
 import { TvSerieModule } from '../tvSeries/tvSerie.module';
 import { ListModule } from '../lists/list.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 export class AppModule implements NestModule {
   static forRoot(): DynamicModule {
     return {
       module: this,
-      imports: [FilmModule, TvSerieModule, ListModule],
+      imports: [
+        FilmModule,
+        TvSerieModule,
+        ListModule,
+        MongooseModule.forRootAsync({
+          useFactory: () => ({
+            uri: 'mongodb://localhost:27017/dreamflicks-lists',
+          }),
+        }),
+      ],
     };
   }
 
