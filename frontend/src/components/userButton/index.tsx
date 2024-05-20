30c35cf2306004768abb9a0e25893d7b7d89b813
Silvia Roldán <silviarf2902@gamilcom>
2024-05-20 21:44:04 +0200

    feat(UserButton): add user functionalities

--- frontend/src/components/userButton/index.tsx
+++ frontend/src/components/userButton/index.tsx
@@ -1,49 +1,90 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
-import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import React, { useEffect, useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { Button, styled } from '@mui/material';
+import { jwtDecode } from 'jwt-decode';
+import CustomButton from '../customButton';
+import Login from '../modals/login';
 
-const StyledAccountCircleIcon = styled(AccountCircleIcon)({
-  color: '#000000',
+const SImg = styled('img')({
+  borderRadius: '50%',
   width: '2.8rem',
-  height: 'auto',
+  height: '2.8rem',
 });
 
-const StyledEmojiEmotionsIcon = styled(EmojiEmotionsIcon)({
+const Text = styled('p')({
+  fontFamily: 'Roboto, sans-serif',
+  fontSize: '1.1rem',
+  fontWeight: 'bold',
+  textDecoration: 'none',
   color: '#000000',
-  width: '2.8rem',
-  height: 'auto',
 });
 
-type CustomButtonProps = {
-  label?: string;
-  onClick?: () => void;
-  registered?: boolean;
-  testId?: string;
-};
+const UserContainer = styled('div')({
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+});
 
-const UserButton = ({
-  label,
-  onClick,
-  registered = false,
-  testId,
-}: CustomButtonProps) => {
-  let icon = <StyledAccountCircleIcon />;
+const UserButton = () => {
+  const [user, setUser] = useState<{ name: string; picture: string } | null>();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-  if (registered) {
-    icon = <StyledEmojiEmotionsIcon />;
-  }
+  useEffect(() => {
+    const token = localStorage.getItem('user');
+    if (token) {
+      try {
+        const decoded: { name: string; picture: string } = jwtDecode(token);
+        setUser(decoded);
+      } catch (error) {
+        console.error('Error decoding token:', error);
+      }
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+    window.location.reload();
+  };
 
   return (
-    <Button
-      style={{ backgroundColor: 'transparent', color: '#000000' }}
-      sx={{ textTransform: 'none' }}
-      onClick={onClick}
-      data-testid={testId}
-      endIcon={icon}
-    >
-      {label}
-    </Button>
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin={''}
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      {user ? (
+        <UserContainer>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleLogout}
+            style={{ marginLeft: 8 }}
+          >
+            Logout
+          </Button>
+          <Text>{user.name}</Text>
+          <SImg src={user.picture} alt="User" />
+        </UserContainer>
+      ) : (
+        <>
+          <CustomButton
+            label="Inicia sesión 🎥"
+            onClick={handleOpen}
+            styles={{ border: '1px solid #000000' }}
+          />
+          <Login open={open} handleClose={handleClose} />
+        </>
+      )}
+    </>
   );
 };
 
