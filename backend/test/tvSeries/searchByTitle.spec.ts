571a0b056240afe34e6941547c63837578b82712
Silvia Roldán <silviarf2902@gamilcom>
2024-06-07 18:16:20 +0200

    feat(searchByTitle): add test suite

---
+++ backend/test/tvSeries/searchByTitle.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SearchByTitle } from '../../src/tvSeries/application/searchByTitle';
+import { TvSerieRepository } from '../../src/tvSeries/domain/repository/tvSerieRepository';
+import { TvSerie } from '../../src/tvSeries/domain/entities/tvSerie';
+
+describe('SearchById test', () => {
+  let searchByTitle: SearchByTitle;
+  let TvSerieRepositoryMock: Partial<TvSerieRepository>;
+
+  beforeEach(async () => {
+    TvSerieRepositoryMock = {
+      search: jest.fn(),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SearchByTitle,
+        { provide: TvSerieRepository, useValue: TvSerieRepositoryMock },
+      ],
+    }).compile();
+
+    searchByTitle = module.get<SearchByTitle>(SearchByTitle);
+  });
+
+  const series = [
+    new TvSerie(1, 'title', 'poster', 'releaseDate'),
+    new TvSerie(2, 'title', 'poster', 'releaseDate'),
+  ];
+  const query = 'title';
+
+  it('should return a list of tvSeries that follows the query', async () => {
+    jest.spyOn(TvSerieRepositoryMock, 'search').mockResolvedValue(series);
+
+    const expectedSeries = await searchByTitle.run(query);
+
+    expect(expectedSeries).toBe(series);
+    expect(TvSerieRepositoryMock.search).toHaveBeenCalled();
+  });
+
+  it('should throw an error', async () => {
+    jest
+      .spyOn(TvSerieRepositoryMock, 'search')
+      .mockRejectedValue(new Error('error'));
+
+    await expect(searchByTitle.run(query)).rejects.toThrow('error');
+    expect(TvSerieRepositoryMock.search).toHaveBeenCalled();
+  });
+});
