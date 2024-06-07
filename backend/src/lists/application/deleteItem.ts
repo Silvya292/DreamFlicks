51f42fe59404f2ebb7556a806ac609d9e682f819
Silvia Roldán <silviarf2902@gamilcom>
2024-06-07 18:47:36 +0200

    chore(deleteItem):remove unused imports

--- backend/src/lists/application/deleteItem.ts
+++ backend/src/lists/application/deleteItem.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ListRepository } from '../domain/repository/listRepository';
-import { ListItem } from '../domain/entities/item.interface';
 
 @Injectable()
 export class DeleteItem {
